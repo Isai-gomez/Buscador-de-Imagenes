bdabeaeca3ed86aa95842f2c7398eed76eea138e
Carlos Alberto <Carlos@MacBook-Pro-de-Carlos.local>
2019-10-22 15:16:44 -0500

    Leer datos y pasarlos por props
    En el buscador se estan leyendo los datos y se estan pasando por props al componente principal.

--- reactimagenes/src/App.js
+++ reactimagenes/src/App.js
@@ -4,12 +4,22 @@ import Buscador from './componentes/Buscador';
 
 class App extends Component {
 
+  state = {
+    termino: '',
+  }
+
+  datosBusqueda = (termino) => {
+    console.log(termino);
+  }
+
   render() {
     return(
       <div className="app container">
         <div className="jumbotron">
             <p className="lead text-center">Buscador de Imágenes</p>
-            <Buscador />
+            <Buscador 
+              datosBusqueda={this.datosBusqueda}
+            />
         </div>
       </div>
     );
--- reactimagenes/src/componentes/Buscador.js
+++ reactimagenes/src/componentes/Buscador.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 
 class Buscador extends Component {
     
+    busquedaRef = React.createRef();
+
+    obtenerDatos = (e) => {
+        e.preventDefault();
+
+        //leer el valor
+        const termino = this.busquedaRef.current.value;
+
+        // console.log(termino);
+        this.props.datosBusqueda(termino);
+    }
+
     render() { 
         return ( 
-            <form>
+            <form onSubmit={this.obtenerDatos}>
                 <div className="row">
                     <div className="form-group col-md-8">
-                        <input className="form-control form-control-lg" type="text" placeholder="Busca tú imagén, ejemplo: Futbol"></input>
+                        <input ref={this.busquedaRef} className="form-control form-control-lg" type="text" placeholder="Busca tú imagén, ejemplo: Futbol"></input>
                     </div>
                     <div className="form-group col-md-4">
                         <input type="submit" className="btn btn-lg btn-danger btn-block" value="Buscar..."></input>
