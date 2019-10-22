9942bea1212e1cb2dd28a6aa485b503e51fef125
Carlos Alberto <Carlos@MacBook-Pro-de-Carlos.local>
2019-10-22 16:42:05 -0500

    Update Imagen.js
    Actualizacion del componente imagen

--- reactimagenes/src/componentes/Imagen.js
+++ reactimagenes/src/componentes/Imagen.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
 const Imagen = (props) => {
+
+    const {largeImageURL, likes, previewURL, tags, views} = props.imagen;
+
     return ( 
-        <h1>Desde imagen</h1>
+        <div className="col-6 col-sm-4 col-md-3 mb-4">
+            <div className="card">
+                <img className="card-img-top" rsc={previewURL} alt={tags}></img>
+                <div className="card-body">
+                    <p className="card-text">{likes} Me gusta</p>
+                    <p className="card-text">{views} Vistas</p>
+                    <a href={largeImageURL} target="_blank" className="btn btn-primary btn-block">Ver Imagen</a>
+                </div>
+            </div>
+        </div>
      );
 }
  
